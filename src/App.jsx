00b9559b494d09ea513ad33b47ab6577d1684b9c
Bolaji <akinloyebolaji7@gmail.com>
2025-08-07 06:29:25 -0700

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { LogIn } from "lucide-react";
 import Landing from "./routes/Landing";
 import {Routes, Route, Navigate} from 'react-router-dom'
 import Signup from "./routes/Signup";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+    <div className="min-h-screen bg-slate-50">
+    <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+    </div>
       </>
   );
 }
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,6 +1,6 @@
 function Login(){
     return(
-        <h1>Sign Up</h1>
+        <h1>Login</h1>
     )
 }
 export default Login;
